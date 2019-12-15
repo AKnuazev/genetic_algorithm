44af5d53ad8aac13c5614016582e980092575b24
AKnuazev <2536040@gmail.com>
2019-12-15 18:28:01 +0300

    Modified code quality

--- source/genetic_algorithm.cpp
+++ source/genetic_algorithm.cpp
@@ -43,9 +43,9 @@ void PrintPopulation(int population_num, std::vector<Individ> individs)
 	std::cout << std::fixed << std::setprecision(3);
 
 	std::cout << "Population:" << std::endl;
-	std::cout << " _________________________________________________________________________________________________________ \n";
-	std::cout << "|  Population number |  Gen 1 (X)  |  Gen 2 (Y)  |   FIT value  |   Max FIT value  |   Average FIT value  |\n";
-	std::cout << "|--------------------|-------------|-------------|--------------|------------------|----------------------|\n";
+	std::cout << " _________________________________________________________________________________________________________ " << std::endl;
+	std::cout << "|  Population number |  Gen 1 (X)  |  Gen 2 (Y)  |   FIT value  |   Max FIT value  |   Average FIT value  |" << std::endl;
+	std::cout << "|--------------------|-------------|-------------|--------------|------------------|----------------------|" << std::endl;
 	for (int i = 0; i < 4; i++)
 	{
 		// Print population number
@@ -55,7 +55,7 @@ void PrintPopulation(int population_num, std::vector<Individ> individs)
 			if (population_num / 10 < 1)
 				std::cout << " ";
 			std::cout << "         " << population_num << "         |    ";
-			
+
 		}
 		else
 			std::cout << "|                    |    ";
@@ -67,9 +67,9 @@ void PrintPopulation(int population_num, std::vector<Individ> individs)
 		std::cout << individs[i].x << "   |    ";
 
 		if (individs[i].y > 0)
-			std::cout << " "; 
+			std::cout << " ";
 
-		std::cout<< individs[i].y << "   |     ";
+		std::cout << individs[i].y << "   |     ";
 
 		// Print Fit values
 		std::cout << FitFunc(individs[i]) << "    |       ";
@@ -85,21 +85,26 @@ void PrintPopulation(int population_num, std::vector<Individ> individs)
 
 		std::cout << std::endl;
 	}
-	std::cout << "|____________________|_____________|_____________|______________|__________________|______________________|\n";
+	std::cout << "|____________________|_____________|_____________|______________|__________________|______________________|" << std::endl;
 }
 
-void PrintRoulette(std::vector<float> sectors)
+void PrintRoulette(std::vector<float> fit_values)
 {
-	std::cout << std::fixed << std::setprecision(2);
+	std::cout << std::fixed << std::setprecision(1);
 
 	std::cout << "Rullete sectors:" << std::endl;
-	std::cout << " ___________________________________ " << std::endl;
-	std::cout << "|        |        |        |        |" << std::endl;
+	std::cout << " ___________________________________________ " << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
 	std::cout << "|  ";
-	for (int i = 0; i < 5; i++)
-		std::cout << sectors[i] << "  |  ";
+	for (auto value : fit_values)
+	{
+		if (value * 100 < 10)
+			std::cout << " ";
+
+		std::cout << value * 100 << " %  |  ";
+	}
 	std::cout << std::endl;
-	std::cout << "|________|________|________|________| " << std::endl;
+	std::cout << "|__________|__________|__________|__________| " << std::endl;
 }
 
 void Normalize(std::vector<float>& values)
@@ -115,7 +120,7 @@ void Normalize(std::vector<float>& values)
 
 void Mutation(Individ& individ)
 {
-	int do_mutation = (int)rand()%4;
+	int do_mutation = (int)rand() % 4;
 	bool mutated_gen_num = 0;
 	float mutation_value = 0;
 
@@ -123,7 +128,7 @@ void Mutation(Individ& individ)
 	{
 		mutated_gen_num = rand() % 2;
 		mutation_value = ((float)rand() / RAND_MAX)*0.2 - 0.1;
-		
+
 		if (mutated_gen_num == 0)
 			individ.x += mutation_value;
 		else
@@ -136,9 +141,8 @@ void Crossover(std::vector<Individ>& individs, std::vector<int> selected)
 	std::vector<Individ> new_population;
 	Individ child1, child2;
 
-	for (int i = 0; i < 4; i++)
-		if (std::find(selected.begin(), selected.end(), i) == selected.end()) // Not found
-			new_population.push_back(individs[i]);
+	for (auto number : selected)
+		new_population.push_back(individs[number]);
 
 	child1.x = individs[selected[0]].x;
 	child1.y = individs[selected[1]].y;
@@ -160,37 +164,37 @@ void SelectionAndCrossover(std::vector<Individ>& individs, int population_num)
 	std::vector<int> selected;
 
 	// Check values
-	for (int i = 0; i < individs.size(); i++)
-		fit_values.push_back(FitFunc(individs[i]));
+	for (auto indiv : individs)
+		fit_values.push_back(FitFunc(indiv));
 
 	// Normalize
 	Normalize(fit_values);
-	
+
 	// Make sectors
 	std::vector<float> sectors;
 	float prev = 0;
 
 	sectors.push_back(0);
-	for (int i = 0; i < 4; i++)
+	for (auto fit_value : fit_values)
 	{
-		std::cout << "Prev: " << prev << "; FIT value: " << fit_values[i] << std::endl;
-		sectors.push_back(prev + fit_values[i]);
-		prev += fit_values[i];
+		sectors.push_back(prev + fit_value);
+		prev += fit_value;
 	}
 
-	//Print population info
+	// Print population info
 	std::cout << "POPULATION " << population_num << std::endl;
 	std::cout << "__________________________________________" << std::endl;
 	PrintPopulation(population_num, individs);
 	std::cout << std::endl;
+
 	// Print sectors
-	PrintRoulette(sectors);
+	PrintRoulette(fit_values);
 	std::cout << std::endl;
 
 	// Make rolls
 	float roll_1 = (float)rand() / RAND_MAX;
 	float roll_2 = (float)rand() / RAND_MAX;
-	
+
 	for (int i = 0; i < 4; i++)
 	{
 		if (sectors[i] < roll_1 && roll_1 < sectors[i + 1])
@@ -199,21 +203,20 @@ void SelectionAndCrossover(std::vector<Individ>& individs, int population_num)
 		if (sectors[i] < roll_2 && roll_2 < sectors[i + 1])
 			selected.push_back(i);
 	}
-	
+
 	// Crossover
 	Crossover(individs, selected);
 	std::cout << std::endl << std::endl << std::endl << std::endl;
 
 }
 
-
 int main()
 {
 	std::vector<Individ> population;
 	const int N = 50;  // number of populations
-	
-	//srand(time(NULL));
-	
+
+	srand(time(NULL));
+
 	for (int i = 0; i < 4; i++)
 	{
 		Individ* curr_indiv = new Individ();
@@ -225,6 +228,7 @@ int main()
 	for (int i = 0; i < N; i++)
 	{
 		SelectionAndCrossover(population, i);
-	} 
-	std::cout << "Best:" << FitFunc(population[0]) << std::endl;
+	}
+	std::cout << std::fixed << std::setprecision(3);
+	std::cout << "Result: " << FitFunc(*std::max_element(population.begin(), population.end(), CompareByFitFunc)) << std::endl;
 }
